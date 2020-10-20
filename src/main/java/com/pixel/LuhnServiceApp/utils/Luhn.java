b1db54c171ad7613ac87c0e44713ab6f8a6d16eb
Bartosz <bartoszpawlikowsky@gmail.com>
2020-10-20 18:47:08 +0200

    Added utils.Luhn class for calculating the control digit and to verify is last digit is the control digit of the preceding number

---
+++ src/main/java/com/pixel/LuhnServiceApp/utils/Luhn.java
@@ -0,0 +1,73 @@
+package com.pixel.LuhnServiceApp.utils;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Luhn {
+    private static final int WEIGHT_1 = 1;
+    private static final int WEIGHT_2 = 2;
+
+    public static Boolean luhnVerifyNumber(String number) {
+        if(number.isEmpty())
+            return false;
+        if(number.contains(" ")) {
+            StringBuilder newNumber = new StringBuilder();
+            for (char c : number.toCharArray()) {
+                if(Character.isDigit(c))
+                    newNumber.append(c);
+            }
+            number = newNumber.toString();
+        }
+        String numberBeingChecked = number.substring(0, number.length()-1);
+        String expectedDigit = String.valueOf(number.charAt(number.length()-1));
+
+        int sum = luhnGenerateSum(numberBeingChecked);
+
+        int control = luhnControlDigit(sum);
+        if((sum + control)%10 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    private static Integer luhnGenerateSum(String parseNumber) {
+
+        int sum = 0;
+        int digits = 0;
+
+        for (int i = parseNumber.length() - 1; i >= 0; i--) {
+            char c = parseNumber.charAt(i);
+
+            if (Character.isDigit(c)) {
+                digits++;
+                int digit = Integer.parseInt(String.valueOf(c));
+                int weight;
+
+                if((digits+1)%2 != 0)
+                    weight = WEIGHT_1;
+                else
+                    weight = WEIGHT_2;
+                digit = parseDigit(digit * weight);
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+
+    private static int parseDigit(int digit){
+
+        if(digit>9){
+            String digitToStr = String.valueOf(digit);
+            int lowerDigit=Integer.parseInt(String.valueOf(digitToStr.charAt(1)));
+            int higherDigit=Integer.parseInt(String.valueOf(digitToStr.charAt(0)));
+            digit = lowerDigit + higherDigit;
+        }
+        return digit;
+    }
+
+    private static int luhnControlDigit(int sum) {
+        return (sum%10 == 0) ? 0 : 10-(sum%10);
+    }
+}
