762f968441b76b2882953a89c283b06fac31a2d9
Bartosz <bartoszpawlikowsky@gmail.com>
2020-10-20 17:57:23 +0200

    First commit: Added A SpringBoot starter project with Loombok and Spring Web

---
+++ src/main/java/com/pixel/LuhnServiceApp/LuhnServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.pixel.LuhnServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuhnServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuhnServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixel/LuhnServiceApp/LuhnServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixel.LuhnServiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuhnServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
